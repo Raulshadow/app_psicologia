72184fadbfda60aeca466a9e4b6e8bd9f749d84a
Raulshadow <50181593+Raulshadow@users.noreply.github.com>
2022-08-29 10:31:51 -0300

    Botões
    Adição de botões para a coluna, ainda não possui eventos mas ja possui os botões, oque falta é colocar ações, ícone e melhorar o visual.

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,56 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        body:Column(
-          children: [
-            Row(// linha 1
-              children: [
-                Text('Linha 1')
-              ],
-            ),
-            Row( // linha 2
-              children: [
-                Text('Linha 2')
-              ],
-            ),
-          ],
-        ) ,
         appBar: AppBar(
           title: Text('App Ingresso'),
           centerTitle: true,
           backgroundColor: Colors.purple[500],
         ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Row(
+              // linha 1
+              children: [
+                Expanded(
+                  child: Container(
+                    height: 75,
+                    child: FlatButton(
+                      color: Colors.red,
+                      onPressed: () {},
+                      child: Text ("Ingresso 1"),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+
+            Row(
+              // linha 1
+              children: [
+                Expanded(
+                  child: Container(
+                    height: 75,
+                    child: FlatButton(
+                      color: Colors.green,
+                      onPressed: () {},
+                      child: Text ("Ingresso 2"),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
