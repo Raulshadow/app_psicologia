841a3dcb75102c682c6fecbbb229973f651d235f
Luiz Felipe <luizfelipesantospereira01@gmail.com>
2022-11-16 19:57:20 -0300

    Transformado itens da lista em map

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,27 @@ void main() {
   runApp(MaterialApp(home: MyApp()));
 }
 
+const filmes = [
+  {
+    'image':
+        'https://i.pinimg.com/originals/0d/0d/0d/0d0d0d0d0d0d0d0d0d0d0d0d0d0d0d0d.jpg',
+    'title': 'O Poderoso Chefão',
+    'category': 'Drama',
+  },
+  {
+    'image':
+        'https://i.pinimg.com/originals/0d/0d/0d/0d0d0d0d0d0d0d0d0d0d0d0d0d0d0d0d.jpg',
+    'title': 'O Poderoso Chefão',
+    'category': 'Drama',
+  },
+  {
+    'image':
+        'https://i.pinimg.com/originals/0d/0d/0d/0d0d0d0d0d0d0d0d0d0d0d0d0d0d0d0d.jpg',
+    'title': 'O Poderoso Chefão',
+    'category': 'Drama',
+  }
+];
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -15,7 +36,8 @@ class MyApp extends StatelessWidget {
       appBar: AppBar(
         leading: GestureDetector(
           onTap: () {
-            Navigator.push(context, MaterialPageRoute(builder:  (context) => Categorias()));
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => Categorias()));
           },
           child: const Icon(
             Icons.menu,
@@ -27,199 +49,76 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.purple[500],
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          // linha 1
-          Row(
-            children: [
-              Expanded(
-                child: Container(
-                  margin: const EdgeInsets.fromLTRB(5.0, 5.0, 5.0, 0.0),
-                  height: 75,
-                  decoration: BoxDecoration(
-                    color: Colors.white24,
-                    border: Border.all(
-                      color: Colors.deepPurpleAccent,
-                      width: 2,
-                      style: BorderStyle.solid,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: filmes.map((filme) {
+            return Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    margin: const EdgeInsets.fromLTRB(5.0, 5.0, 5.0, 0.0),
+                    height: 75,
+                    decoration: BoxDecoration(
+                      color: Colors.white24,
+                      border: Border.all(
+                        color: Colors.deepPurpleAccent,
+                        width: 2,
+                        style: BorderStyle.solid,
+                      ),
+                      borderRadius: BorderRadius.circular(8),
                     ),
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                  child: TextButton(
-                    onPressed: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) => Filmes()));
-                    },
-                    style:
-                        TextButton.styleFrom(backgroundColor: Colors.blue[50]),
-                    child: Container(
-                      alignment: Alignment.centerLeft,
-                      margin: const EdgeInsets.fromLTRB(0.0, 5.0, 10.0, 5.0),
-                      child: Row(
-                        children: [
-                          Container(
-                            width: 50,
-                            height: 50,
-                            padding: const EdgeInsets.all(5),
-                            decoration: BoxDecoration(
-                                color: Colors.white,
-                                border: Border.all(
-                                  color: Colors.deepPurpleAccent,
-                                  width: 1.5,
-                                  style: BorderStyle.solid,
-                                ),
-                                borderRadius: BorderRadius.circular(10),
-                                boxShadow: const [
-                                  BoxShadow(
-                                    color: Colors.blue,
-                                    blurRadius: 2,
-                                  )
-                                ]),
-                            child: const Image(
-                              image: NetworkImage(
-                                  'https://cdn-icons-png.flaticon.com/512/4406/4406665.png'),
+                    child: TextButton(
+                      onPressed: () {
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (context) => Filmes()));
+                      },
+                      style: TextButton.styleFrom(
+                          backgroundColor: Colors.blue[50]),
+                      child: Container(
+                        alignment: Alignment.centerLeft,
+                        margin: const EdgeInsets.fromLTRB(0.0, 5.0, 10.0, 5.0),
+                        child: Row(
+                          children: [
+                            Container(
+                              width: 50,
+                              height: 50,
+                              padding: const EdgeInsets.all(5),
+                              decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  border: Border.all(
+                                    color: Colors.deepPurpleAccent,
+                                    width: 1.5,
+                                    style: BorderStyle.solid,
+                                  ),
+                                  borderRadius: BorderRadius.circular(10),
+                                  boxShadow: const [
+                                    BoxShadow(
+                                      color: Colors.blue,
+                                      blurRadius: 2,
+                                    )
+                                  ]),
+                              child: const Image(
+                                image: NetworkImage(
+                                    'https://cdn-icons-png.flaticon.com/512/4406/4406665.png'),
+                              ),
                             ),
-                          ),
-                          const Text(
-                            '   Ingresso 1',
-                            style: TextStyle(color: Colors.black),
-                          )
-                        ],
+                            Padding(
+                              padding: const EdgeInsets.only(
+                                  left: 15, bottom: 20, right: 20, top: 10),
+                              child: Text(filme['title']!,
+                                  style: const TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 16,
+                                  )),
+                            )
+                          ],
+                        ),
                       ),
                     ),
                   ),
                 ),
-              ),
-            ],
-          ),
-          // linha 2
-          Row(
-            children: [
-              Expanded(
-                child: Container(
-                  margin: const EdgeInsets.fromLTRB(5.0, 5.0, 5.0, 0.0),
-                  height: 75,
-                  decoration: BoxDecoration(
-                    color: Colors.white24,
-                    border: Border.all(
-                      color: Colors.deepPurpleAccent,
-                      width: 2,
-                      style: BorderStyle.solid,
-                    ),
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                  child: TextButton(
-                    onPressed: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) => Filmes()));
-                    },
-                    style:
-                        TextButton.styleFrom(backgroundColor: Colors.blue[50]),
-                    child: Container(
-                      alignment: Alignment.centerLeft,
-                      margin: const EdgeInsets.fromLTRB(0.0, 5.0, 10.0, 5.0),
-                      child: Row(
-                        children: [
-                          Container(
-                            width: 50,
-                            height: 50,
-                            padding: const EdgeInsets.all(5),
-                            decoration: BoxDecoration(
-                                color: Colors.white,
-                                border: Border.all(
-                                  color: Colors.deepPurpleAccent,
-                                  width: 1.5,
-                                  style: BorderStyle.solid,
-                                ),
-                                borderRadius: BorderRadius.circular(10),
-                                boxShadow: const [
-                                  BoxShadow(
-                                    color: Colors.blue,
-                                    blurRadius: 2,
-                                  )
-                                ]),
-                            child: const Image(
-                              image: NetworkImage(
-                                  'https://cdn-icons-png.flaticon.com/512/4406/4406665.png'),
-                            ),
-                          ),
-                          const Text(
-                            '   Ingresso 2',
-                            style: TextStyle(color: Colors.black),
-                          )
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-          // linha 3
-          Row(
-            children: [
-              Expanded(
-                child: Container(
-                  margin: const EdgeInsets.fromLTRB(5.0, 5.0, 5.0, 0.0),
-                  height: 75,
-                  decoration: BoxDecoration(
-                    color: Colors.white24,
-                    border: Border.all(
-                      color: Colors.deepPurpleAccent,
-                      width: 2,
-                      style: BorderStyle.solid,
-                    ),
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                  child: TextButton(
-                    onPressed: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) => Filmes()));
-                    },
-                    style:
-                        TextButton.styleFrom(backgroundColor: Colors.blue[50]),
-                    child: Container(
-                      alignment: Alignment.centerLeft,
-                      margin: const EdgeInsets.fromLTRB(0.0, 5.0, 10.0, 5.0),
-                      child: Row(
-                        children: [
-                          Container(
-                            width: 50,
-                            height: 50,
-                            padding: const EdgeInsets.all(5),
-                            decoration: BoxDecoration(
-                                color: Colors.white,
-                                border: Border.all(
-                                  color: Colors.deepPurpleAccent,
-                                  width: 1.5,
-                                  style: BorderStyle.solid,
-                                ),
-                                borderRadius: BorderRadius.circular(10),
-                                boxShadow: const [
-                                  BoxShadow(
-                                    color: Colors.blue,
-                                    blurRadius: 2,
-                                  )
-                                ]),
-                            child: const Image(
-                              image: NetworkImage(
-                                  'https://cdn-icons-png.flaticon.com/512/4406/4406665.png'),
-                            ),
-                          ),
-                          const Text(
-                            '   Ingresso 3',
-                            style: TextStyle(color: Colors.black),
-                          )
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ],
-      ),
+              ],
+            );
+          }).toList()),
       floatingActionButton: FloatingActionButton.extended(
         onPressed: () {
           Navigator.push(
