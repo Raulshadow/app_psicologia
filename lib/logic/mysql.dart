46d2ae5bc729a130caead568dc339b1f822734bc
Luiz Felipe <luizfelipesantospereira01@gmail.com>
2022-11-19 20:50:40 -0300

    Adicionado mysql.dart para gitignore

--- lib/logic/mysql.dart
+++ lib/logic/mysql.dart
@@ -3,7 +3,7 @@ import 'package:mysql_client/mysql_client.dart';
 class Mysql {
    String host = '10.0.2.2',
                 user = 'root',
-                password = 'senhabd123.',
+                password = 'admin',
                 db = 'projeto';
    int port = 3306;
 
